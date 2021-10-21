605595645494b7d782190f321811ef1541561876
root <root@DESKTOP-H3T39UL.localdomain>
2021-10-21 15:35:30 -0300

    Creating a global reset with styled components

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,13 +1,8 @@
 import styled from 'styled-components';
 
-// Assim que usamos o styled. -> Temos acesso a todas as tags html's
-// Styled-components já entende que estamos passando um { children }
 const Titulo = styled.h1`
   color: grey;
   padding: 25px 0;
 `;
 
-// const Titulo = ({ children }) => {
-//   return <h1 className="titulo">{children}</h1>;
-// };
 export default Titulo;
