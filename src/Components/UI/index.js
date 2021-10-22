20ced3eb2d73047d9ae75f2d4ef6238740b7de89
root <root@DESKTOP-H3T39UL.localdomain>
2021-10-21 23:47:35 -0300

    Creating the stylized components that will be used in other parts of our application, refactoring the div and section in the Container component

---
+++ src/Components/UI/index.js
@@ -0,0 +1,7 @@
+// Criando os componentes estilizados que serão aproveitados em outras partes da nossa aplicação
+import styled from 'styled-components';
+
+export const Icon = styled.img`
+  height: 25px;
+  width: 25px;
+`;
