1858a8f756231ea6aeb78ea02ca330296abf09c7
root <root@DESKTOP-H3T39UL.localdomain>
2021-10-22 22:44:58 -0300

    Using our theme props

--- src/Components/UI/variables.js
+++ src/Components/UI/variables.js
@@ -5,5 +5,5 @@ export const lightBackground = "#f1f1f1"
 export const textLightBackground = "#5c5b5e"
 
 export const darkContent = "#5c5b5e"
-export const darkBackground = "#5c5b5e"
+export const darkBackground = "#363537"
 export const textDarkBackground = "#FAFAFA"
